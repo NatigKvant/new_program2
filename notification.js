68d195b28049b6837d0fbc0df9500759ea2cd760
Natig <natikibishov@gmail.com>
2019-01-18 17:17:08 +0300

    Second Commit

---
+++ notification.js
@@ -0,0 +1,11 @@
+ function Hide() {
+            var str = document.getElementById('hide');
+            if (str.style.display === 'none') {
+                str.style.display = 'block';
+            } else {
+        }
+        str.style.display = 'none';
+        }
+        
+        
+        
---
+++ slider.js
@@ -0,0 +1,9 @@
+document.getElementById("switch").onclick = function() {
+  document.getElementById("slider").classList.toggle("change");
+  document.getElementById("circle").classList.toggle("change");
+  document.getElementById("circle-content").classList.toggle("change");
+};
+
+    
+    
+    
